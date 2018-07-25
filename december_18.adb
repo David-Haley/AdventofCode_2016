23b7f15a9ce5a9aeb0404a2371c70c941204503b
David-Haley <34273510+David-Haley@users.noreply.github.com>
2018-07-25 21:09:49 +1000

    IChanged to fixed length strings and clean up to improve speed

--- december_18.adb
+++ december_18.adb
@@ -1,72 +1,73 @@
 with Ada.Text_IO; use Ada.Text_IO;
 with Ada.Strings; use Ada.Strings;
 with Ada.Strings.Maps; use Ada.Strings.Maps;
+with Ada.Strings.Fixed; use Ada.Strings.Fixed;
 with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
 with Ada.Text_IO.Unbounded_IO; use Ada.Text_IO.Unbounded_IO;
 with GNAT.MD5; use GNAT.MD5;
 
 procedure December_18 is
 
-   Safe : constant Character := '.';
-   Trap : constant Character := '^';
-
-   subtype Tiles is Character
-     with Static_Predicate => Tiles in Safe | Trap;
-
    Input_File : File_Type;
    My_Input : Unbounded_String;
 
-   subtype Tile_Indices is Positive;
-
-   function New_Tile (Row : in Unbounded_String; Index : in Tile_Indices)
-                      return Tiles is
-
-      Left, Centre, Right : Tiles;
-
-   begin -- New_Tile
-      if Index = Tile_Indices'First then
-         Left := Safe;
-      else
-         Left := Element (Row, Index - 1);
-      end if; -- Index = Tile_Indices'First
-      if Index = Length (Row) then
-         Right := Safe;
-      else
-         Right := Element (Row, Index + 1);
-      end if; -- Index = Length (Row)
-      Centre := Element (Row, Index);
-      if Left = Trap and Centre = Trap and Right /= Trap then
-         return Trap;
-      elsif Left /= Trap and Centre = Trap and Right = Trap then
-         return Trap;
-      elsif Left = Trap and Centre /= Trap and Right /= Trap then
-         return Trap;
-      elsif  Left /= Trap and Centre /= Trap and Right = Trap then
-         return Trap;
-      else
-         return Safe;
-      end if; -- apply rules
-   end New_Tile;
-
    function Count_Safe (My_Input : in Unbounded_String;
-                       Last_Row : in Positive) return Natural is
+                        Last_Row : in Positive) return Natural is
 
-      Current_Row : Unbounded_String := My_Input;
-      Next_Row : Unbounded_String;
+      Safe : constant Character := '.';
+      Trap : constant Character := '^';
+
+      subtype Tiles is Character
+        with Static_Predicate => Tiles in Safe | Trap;
+
+      subtype Tile_Indices is Positive range 1 .. Length (My_Input);
+
+      subtype Row_Strings is String (Tile_Indices'Range);
+      Current_Row : Row_Strings := To_String (My_Input);
+      Next_Row : Row_Strings;
       Number_Safe : Natural;
       subtype Rows is Positive range 2 .. Last_Row;
       -- first row is My_Input
       Safe_Set : Character_Set := To_Set (Safe);
 
+      function New_Tile (Row : in Row_Strings; Index : in Tile_Indices)
+                      return Tiles is
+
+         Left, Centre, Right : Tiles;
+
+      begin -- New_Tile
+         if Index = Tile_Indices'First then
+            Left := Safe;
+         else
+            Left := Row (Index - 1);
+         end if; -- Index = Tile_Indices'First
+         if Index = Tile_Indices'Last then
+            Right := Safe;
+         else
+            Right := Row (Index + 1);
+         end if; -- Index = Tile_Indices'Last
+         Centre := Row (Index);
+         if Left = Trap and Centre = Trap and Right /= Trap then
+            return Trap;
+         elsif Left /= Trap and Centre = Trap and Right = Trap then
+            return Trap;
+         elsif Left = Trap and Centre /= Trap and Right /= Trap then
+            return Trap;
+         elsif  Left /= Trap and Centre /= Trap and Right = Trap then
+            return Trap;
+         else
+            return Safe;
+         end if; -- apply rules
+      end New_Tile;
+
    begin -- Count_Safe
-      Number_Safe := Ada.Strings.Unbounded.Count (Current_Row, Safe_Set);
+      Number_Safe := Ada.Strings.Fixed.Count (Current_Row, Safe_Set);
       for Row in Rows loop
-         Next_Row := Null_Unbounded_String;
-         for Index in Tile_Indices'First .. Length (Current_Row) loop
-            Append (Next_Row, New_Tile (Current_Row, Index));
-         end loop; -- Index in Tile_Indices'First .. Length (Current_Row)
+         for Index in Tile_Indices loop
+            Next_Row (Index) := New_Tile (Current_Row, Index);
+         end loop; -- Index in Tile_Indices
          Number_Safe := Number_Safe +
-           Ada.Strings.Unbounded.Count (Next_Row, Safe_Set);
+           Ada.Strings.Fixed.Count (Next_Row, Safe_Set);
          Current_Row := Next_Row;
       end loop; -- Row in Rows
       return Number_Safe;
