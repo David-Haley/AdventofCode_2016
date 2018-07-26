a3b296cf30fe3537dcdfe3c1d53cd6f9aa605201
David-Haley <34273510+David-Haley@users.noreply.github.com>
2018-07-26 22:07:51 +1000

    Solution to part one can ce implenented using singly  linked circular list however part two requires double linking

--- december_19.adb
+++ december_19.adb
@@ -1,5 +1,6 @@
 with Ada.Text_IO; use Ada.Text_IO;
 with Ada.Assertions; use Ada.Assertions;
+with Ada.Unchecked_Deallocation;
 
 procedure December_19 is
 
@@ -9,50 +10,101 @@ procedure December_19 is
    package Positive_IO is new Ada.Text_IO.Integer_IO (Positive);
    use Positive_IO;
 
-   function Winning_Elf (My_Input : in Positive) return Positive is
+   type Elves;
+   type Elf_Pointers is access all Elves;
+   type Elves is record
+      Number : Positive;
+      Present_Count : Natural;
+      Next_Elf, Previous_Elf : Elf_Pointers;
+   end record; -- Elves
 
-      subtype Elf_Indices is Positive range 1 .. My_input;
-      Elf : array (Elf_Indices) of Natural := (others => 1);
+   Current_Elf : Elf_Pointers;
 
-      Current : Elf_Indices := 1;
-      Left : Elf_Indices;
+   procedure Create_Elf_Circle (My_Input : in Positive;
+                                First_Elf : out Elf_Pointers) is
 
-      procedure Next_Elf (Elf_Index : in out Elf_Indices) is
+      Current_Elf : Elf_Pointers;
 
-      begin -- Next_Elf
-         if Elf_Index < Elf_Indices'Last then
-            Elf_Index := Elf_Index + 1;
+   begin -- Create_Elf_Circle
+      First_Elf := new Elves'(1, 1, null, null);
+      Current_Elf := First_Elf;
+      for I in Positive range 2 .. My_Input loop
+         Current_Elf.Next_Elf := new Elves'(I, 1, null, Current_Elf);
+         Current_Elf := Current_Elf.Next_Elf;
+      end loop; -- I in Positive range 2 .. My_Input
+      -- close circle
+      Current_Elf.Next_Elf := First_Elf;
+      First_Elf.Previous_Elf := Current_Elf;
+   end Create_Elf_Circle;
+
+   procedure Delete_Elf is new Ada.Unchecked_Deallocation (Elves, Elf_Pointers);
+
+   procedure Winning_Elf (Current_Elf : in out Elf_Pointers;
+                          My_Input : in Positive) is
+
+      -- Uses Global My_Input
+
+      Unlink : Elf_Pointers;
+
+   begin -- Winning_Elf
+      while Current_Elf.Next_Elf /= Current_Elf loop
+         -- more than one elf in circle
+         Unlink := Current_Elf.Next_Elf;
+         Current_Elf.Present_Count := Current_Elf.Present_Count +
+           Unlink.Present_Count;
+         Current_Elf.Next_Elf := Unlink.Next_Elf;
+         Current_Elf.Next_Elf.Previous_Elf := Current_Elf;
+         Delete_Elf (Unlink);
+         Current_Elf := Current_Elf.Next_Elf;
+      end loop; -- Current_Elf.Next_Elf /= Current_Elf
+      assert (Current_Elf.Previous_Elf = Current_Elf, "Inconsistent back link");
+      assert (Current_Elf.Present_Count = My_Input, "wrong Present_Count" &
+                Natural'Image (Current_Elf.Present_Count));
+   end Winning_Elf;
+
+   procedure Cross_Table_Winning_Elf (Current_Elf : in out Elf_Pointers;
+                                      My_Input : in Positive) is
+
+      Unlink, Next_to_Unlink : Elf_Pointers;
+      Elf_Count : Natural := My_Input;
+
+   begin -- Cross_Table_Winning_Elf
+      Unlink := Current_Elf;
+      for I in Positive range 1 .. My_Input / 2 loop
+         Unlink := Unlink.Next_Elf;
+      end loop; -- I in Positive range 1 .. Elf_Count / 2
+      while Current_Elf.Next_Elf /= Current_Elf loop
+         -- more than one elf in circle
+         Current_Elf.Present_Count := Current_Elf.Present_Count +
+           Unlink.Present_Count;
+         Unlink.Previous_Elf.Next_Elf := Unlink.Next_Elf;
+         Unlink.Next_Elf.Previous_Elf := Unlink.Previous_Elf;
+         Elf_Count := Elf_Count - 1;
+         if Elf_Count mod 2 = 1 then
+            Next_to_Unlink := Unlink.Next_Elf;
          else
-            Elf_Index := Elf_Indices'First;
-         end if; -- Elf_Index < Elf_Indices'Last
-      end Next_Elf;
-
-   begin --  Winning_Elf
-      loop -- around the circle
-         while Elf (Current) = 0 loop
-            Next_Elf (Current);
-         end loop; -- Elf (Current) = 0
-         Left := Current;
-         Next_Elf (Left);
-         while Elf (Left) = 0 and Left /= Current loop
-            Next_Elf (Left);
-         end loop; --  Elf (Left) = 0 and Left /= Current
-         if Current = Left then
-            assert (Elf (Current) = My_Input, "wrong number of presents" &
-                      Natural'Image (Elf (Current)));
-            return Current;
-            -- only one elf has presents
-         else
-            Elf (Current) := Elf (Current) + Elf (Left);
-            Elf (Left) := 0;
-            Current := Left;
-         end if; -- Current = Left
-      end loop; -- around the circle
-   end  Winning_Elf;
+            Next_to_Unlink := Unlink.Next_Elf.Next_Elf;
+         end if; -- Elf_Count mod 2 = 1
+         Delete_Elf (Unlink);
+         Unlink := Next_to_Unlink;
+         Current_Elf := Current_Elf.Next_Elf;
+      end loop; -- Current_Elf.Next_Elf /= Current_Elf
+      assert (Current_Elf.Previous_Elf = Current_Elf, "Inconsistent back link");
+      assert (Current_Elf.Present_Count = My_Input, "wrong Present_Count" &
+                Natural'Image (Current_Elf.Present_Count));
+   end Cross_Table_Winning_Elf;
 
 begin -- December_19
    Open (Input_File, In_file, "20161219.txt");
    Get (Input_File, My_Input);
    Close (Input_File);
-   Put_Line ("Elf with presents:" & Positive'Image (Winning_Elf (My_Input)));
+   Create_Elf_Circle (My_Input, Current_Elf);
+   Winning_Elf (Current_Elf, My_Input);
+   Put_Line ("Elf with presents:" & Positive'Image (Current_Elf.Number));
+   Delete_Elf (Current_Elf);
+   Create_Elf_Circle (My_Input, Current_Elf);
+   Cross_Table_Winning_Elf (Current_Elf, My_Input);
+   Put_Line ("Part two elf with presents:" &
+               Positive'Image (Current_Elf.Number));
+   Delete_Elf (Current_Elf);
 end December_19;
